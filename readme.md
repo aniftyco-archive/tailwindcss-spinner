dd1bfbaf3bea7a03dd57688c980f881fbbf5835d
Josh Manders <josh@joshmanders.com>
2018-03-26 22:18:26 -0500

    initial commit

---
+++ readme.md
@@ -0,0 +1,41 @@
+# Tailwind CSS Spinner
+> Spinner utility for Tailwind CSS
+
+## Installation
+
+You can install it with yarn
+
+```bash
+yarn add tailwindcss-spinner
+```
+
+## Usage
+
+To get started using the plugin, you can require it into your Tailwind CSS config file
+
+```js
+plugins: [
+  // Other plugins...
+
+  require('tailwindcss-spinner')(),
+],
+```
+
+Now you will have a `.spinner` class available in your CSS.
+
+If you want to customize the spinner, you can pass any combination of the following options (shown with default values).
+
+```js
+plugins: [
+  // Other plugins...
+
+  require('tailwindcss-spinner')({
+    name: 'spinner', // change class name
+    color: 'grey-light', // color from config to make it
+  }),
+],
+```
+
+## Example
+
+![](https://i.imgur.com/UbwGglQ.gif)
---
+++ src/index.js
@@ -0,0 +1,29 @@
+export default ({name = 'spinner', color = 'grey-light'}) => ({addUtilities, config}) => addUtilities({
+  [`.${name}`]: {
+    'color': 'transparent !important',
+    'pointer-events': 'none',
+    'position': 'relative',
+    '&:after': {
+      'animation': 'spinAround 500ms infinite linear',
+      'border': `2px solid ${config(`colors.${color}`)}`,
+      'border-radius': '290486px',
+      'border-right-color': 'transparent',
+      'border-top-color': 'transparent',
+      'content': '""',
+      'display': 'block',
+      'width': '1em',
+      'height': '1em',
+      'left': 'calc(50% - (1em / 2))',
+      'top': 'calc(50% - (1em / 2))',
+      'position': 'absolute !important',
+    },
+  },
+  '@keyframes spinAround': {
+    'from': {
+      'transform': 'rotate(0deg)',
+    },
+    'to': {
+      'transform': 'rotate(359deg)',
+    },
+  },
+});
