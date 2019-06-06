caf7433a4ff382396670df9789c7957af4a6f8ce
Nestor Vera <nestorvera@me.com>
2019-06-06 21:37:53 +0200

    Add support for Tailwind v1.x (#6)
    * Add support for Tailwind v1.x
    
    * Add TravisCI config file
    
    * Update README for v1.0

---
+++ index.js
@@ -0,0 +1,117 @@
+const _ = require('lodash')
+const flatten = require('flat')
+
+
+const FLATTEN_CONFIG = { delimiter: '-', maxDepth: 2 }
+const handleName = (name, className) => {
+  const split = name.split(`${className}-`)
+  const prefixedName = `${split[0]}${prefixNegativeModifiers(className, split[1])}`
+
+  return prefixedName.split('-default').join('')
+}
+const prefixNegativeModifiers = function(base, modifier) {
+  return _.startsWith(modifier, '-')
+    ? `-${base}-${modifier.slice(1)}`
+    : `${base}-${modifier}`
+}
+
+
+module.exports = function () {
+  return function ({
+    addUtilities, addComponents, addBase, addVariant,
+    e, prefix, theme, variants, config,
+  }) {
+    const buildConfig = (themeKey, ...fallbackKeys) => {
+      return buildConfigFromTheme(themeKey, ...fallbackKeys) || buildConfigFromArray(themeKey)
+    }
+    const buildConfigFromTheme = (themeKey, ...fallbackKeys) => {
+      const buildObject = ([ modifier, value ]) => [ modifier, { [themeKey]: value } ]
+      const getThemeSettings = (themeKey, fallbackKeys) => {
+        const [newThemeKey, ...newFallbackKeys] = fallbackKeys || []
+
+        return theme(themeKey, false) || (fallbackKeys.length && getThemeSettings(newThemeKey, [...newFallbackKeys]))
+      }
+
+      const themeSettings = getThemeSettings(themeKey, fallbackKeys)
+      const themeObject = _.isArray(themeSettings) ? _.zipObject(themeSettings, themeSettings) : themeSettings
+      const themeEntries = themeSettings && Object
+        .entries(flatten(themeObject, FLATTEN_CONFIG))
+        .map(entry => buildObject(entry))
+
+      return themeSettings ? _.fromPairs(themeEntries) : false
+    }
+    const buildConfigFromArray = (property) => {
+      const defaultSettings = defaultValues[property]
+      const defaultEntries = defaultSettings && defaultSettings
+        .map(value => ([value, { [property]: value }]))
+
+      return defaultSettings ? _.fromPairs(defaultEntries) : false
+    }
+
+    const buildPluginUtilityObject = ({ color, size, border, speed }) => {
+      return {
+        'position': 'relative',
+        'color': 'transparent !important',
+        'pointer-events': 'none',
+
+        '&::after': {
+          'content': `''`,
+
+          'position': 'absolute !important',
+          'top': `calc(50% - (${size} / 2))`,
+          'left': `calc(50% - (${size} / 2))`,
+
+          'display': 'block',
+          'width': size,
+          'height': size,
+
+          'border': `${border} solid ${color}`,
+          'border-radius': '9999px',
+          'border-right-color': 'transparent',
+          'border-top-color': 'transparent',
+
+          'animation': `spinAround ${speed} infinite linear`,
+        },
+      }
+    }
+
+    const buildDefaultValuesObject = (defaultConfig, themeKey, ...fallbackKeys) => {
+      const defaultEntries = Object.entries(theme(themeKey, { default: defaultConfig }))
+        .map(([ modifier, config ]) => [ modifier, buildPluginUtilityObject({ ...defaultConfig, ...config }) ])
+
+      return _.fromPairs(defaultEntries)
+    }
+
+    const defaultValues = {}
+    const defaultConfig = {
+      color: 'currentColor',
+      size: '1em',
+      border: '2px',
+      speed: '500ms',
+    }
+
+    const pluginUtilities = {
+      spinner: buildDefaultValuesObject(defaultConfig, 'spinner'),
+    }
+
+    Object.entries(pluginUtilities)
+      .filter(([ modifier, values ]) => !_.isEmpty(values))
+      .forEach(([ modifier, values ]) => {
+        const className = _.kebabCase(modifier)
+        const variantName = Object.keys(Object.entries(values)[0][1])[0]
+        const utilities = flatten({ [`.${e(`${className}`)}`]: values }, FLATTEN_CONFIG)
+
+        addUtilities(
+          _.mapKeys(utilities, (value, key) => handleName(key, className)),
+          variants(variantName, ['responsive'])
+        )
+      })
+
+    addUtilities({
+      '@keyframes spinAround': {
+        'from': { 'transform': 'rotate(0deg)' },
+        'to': { 'transform': 'rotate(360deg)' },
+      },
+    })
+  }
+}
--- readme.md
+++ readme.md
@@ -3,37 +3,77 @@
 
 ## Installation
 
-You can install it with yarn
+Add this plugin to your project:
 
 ```bash
-yarn add tailwindcss-spinner
+# Install using pnpm
+pnpm install --save-dev tailwindcss-spinner
+
+# Install using npm
+npm install --save-dev tailwindcss-spinner
+
+# Install using yarn
+yarn add -D tailwindcss-spinner
 ```
 
 ## Usage
 
-To get started using the plugin, you can require it into your Tailwind CSS config file
-
 ```js
-plugins: [
-  // Other plugins…
-  require('tailwindcss-spinner')(),
-],
+// tailwind.config.js
+{
+  theme: { // defaults to these values
+    spinner: (theme) => ({
+      default: {
+        color: '#dae1e7', // color you want to make the spinner
+        size: '1em', // size of the spinner (used for both width and height)
+        border: '2px', // border-width of the spinner (shouldn't be bigger than half the spinner's size)
+        speed: '500ms', // the speed at which the spinner should rotate
+      },
+      // md: {
+      //   color: theme('colors.red.500', 'red'),
+      //   size: '2em',
+      //   border: '2px',
+      //   speed: '500ms',
+      // },
+    }),
+  },
+
+  variants: { // all the following default to ['responsive']
+    spinner: ['responsive'],
+  },
+
+  plugins: [
+    require('tailwindcss-spinner')(), // no options to configure
+  ],
+}
 ```
 
-Now you will have a `.spinner` class available in your CSS.
+```css
+.spinner {
+  position: relative;
+  color: transparent !important;
+  pointer-events: none;
+}
 
-If you want to customize the spinner, you can pass any combination of the following options (shown with default values).
+.spinner::after {
+  content: '';
+  position: absolute !important;
+  top: calc(50% - (1em / 2));
+  left: calc(50% - (1em / 2));
+  display: block;
+  width: 1em;
+  height: 1em;
+  border: 2px solid currentColor;
+  border-radius: 9999px;
+  border-right-color: transparent;
+  border-top-color: transparent;
+  animation: spinAround 500ms infinite linear;
+}
 
-```js
-plugins: [
-  // Other plugins…
-  require('tailwindcss-spinner')({
-    name: 'spinner', // change class name
-    color: '#dae1e7', // color you want to make the spinner
-    size: '1em', // size of the spinner (used for both width and height)
-    border: '2px', // border-width of the spinner (shouldn't be bigger than half the spinner's size)
-  }),
-],
+@keyframes spinAround {
+  from { transform: rotate(0deg); }
+  to { transform: rotate(360deg); }
+}
 ```
 
 ## Example
